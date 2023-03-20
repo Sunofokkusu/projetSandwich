b8164306a63f5b0c2a8e3cd6aeece735fe8405d3
LePangolin <91209500+LePangolin@users.noreply.github.com>
2023-03-20 12:00:27 +0100

    feat(routrer): sandwiches

--- app/Gateway/router/sandwiches.js
+++ app/Gateway/router/sandwiches.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const axios = require('axios');
 
 
-// METTRE LES ROUTES ICI
+router.get('/', async (req, res) => {
+    let response = await axios.get(process.env.SANDWICHES_ROUTES+ "?fields=*.*")
+    res.json(response.data);
+})
+
+router.get('/:id', async (req, res) => {
+    let response = await axios.get(process.env.SANDWICHES_ROUTES + "?fields=*.*&filter=id,eq," + req.params.id)
+    res.json(response.data);
+})
 
 module.exports = router;
